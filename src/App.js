1bd2c53770b08b9ae350f515ad5c102b1b372ca3
Emrebym <64077223+Emrebym@users.noreply.github.com>
2023-10-19 14:39:22 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import React from 'react';
+import WeatherApp from './components/Weatherapp/WeatherApp';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      
+     <WeatherApp />
+    </div>
+  );
+}
+
+export default App;
